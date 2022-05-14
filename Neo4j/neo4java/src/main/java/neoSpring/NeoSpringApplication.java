d2691d81e8ef86dd2db862f37643eddbc427a772
Tureczek <n.m.tureczek@gmail.com>
2022-05-14 18:03:19 +0200

    Update NeoSpringApplication.java

--- Neo4j/neo4java/src/main/java/neoSpring/NeoSpringApplication.java
+++ Neo4j/neo4java/src/main/java/neoSpring/NeoSpringApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
+// Maybe this works
 @EnableTransactionManagement
 @EnableNeo4jRepositories
 @SpringBootApplication
